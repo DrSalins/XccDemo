aedddfd31cc8806a40e6f7407dfccc4c2676f07e
siddarthmudaliar93@gmail.com <siddarthmudaliar93@gmail.com>
2022-11-29 14:13:18 +0800

    complete

---
+++ app.js
@@ -0,0 +1,21 @@
+//&//
+// const a = 10;
+// if (typeof a === "number" && a === 8) {
+//   console.log("Great");
+// } else {
+//   console.log("Not Great");
+// }
+// //OR
+// const b = "Eleven";
+// if (typeof b === "string" || b === 11) {
+//   console.log("Awesome");
+// } else {
+//   console.log("Not Awesome");
+// }
+// const c = 10;
+// console.log(c == "10"); //Checking Value
+// console.log(c === "10"); //Checking Value and Type
+
+var a = 10;
+var b = "10";
+console.log(a !== b);
